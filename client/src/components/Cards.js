96adff1a38894377f4186ec1a1efbdfc2d3a0c21
Austin Greer <agreer.ag12@gmail.com>
2021-12-11 21:22:30 -0500

    created new Cards component to clean up Search component. Search feature is working as expected. Current code can be merged over and additional work on Cards component can be done.

---
+++ client/src/components/Cards.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import '../styles/cards.scss';
+
+import { StarOutlined, StarFilled, Loading3QuartersOutlined } from '@ant-design/icons';
+import ReactHtmlParser from 'react-html-parser';
+
+function Cards({recipes, loading, likedRecipeHandler, moreInfoHandler }) {
+    
+    const starIconStyle = {
+        fontSize: '2.5rem', 
+        height: '5vh',
+        cursor: 'pointer',
+        color: '#fcde7b',
+    }
+
+    const loadingIconStyle = {
+        fontSize: '3rem',
+        width: '100%'
+    }
+
+    return (
+        <div className="cards-wrapper">
+                { loading && <Loading3QuartersOutlined spin style={loadingIconStyle} />}
+                { recipes && recipes.map(recipe => {
+                    const { id, image, title, readyInMinutes, servings, spoonacularScore, vegan, vegetarian, pricePerServing } = recipe
+                    const description = recipe.summary
+                    return (
+                            <div className="card" key={id}>
+                                <img className="recipe-image" src={image} alt="recipe"/>
+                                <div className="card-content">
+                                    <h2 className="recipe-title">{title}</h2>
+                                    <div className="recipe-info">
+                                        <p>Prep-time < br />{readyInMinutes} Minutes</p>
+                                        <p>Servings < br />{servings}</p>
+                                        <p>Score < br />{spoonacularScore}</p>
+                                    </div>
+                                </div>
+
+                                { recipe.open && (
+                                    <div className='open'>
+                                        <div className='description'>
+                                            <h2>Description</h2>
+                                            <p>{ ReactHtmlParser(description) }</p>
+                                        </div>
+                                    </div>
+                                )}
+
+                                <div className="learn-more">
+                                    {recipe.liked ? (
+                                        <StarFilled style={starIconStyle} onClick={() => likedRecipeHandler(id)} />) : (
+                                        <StarOutlined style={starIconStyle} onClick={() => likedRecipeHandler(id)} /> ) 
+                                    }
+                                    <button onClick={() => moreInfoHandler(id)}>{ !recipe.open ? 'More Info' : 'Less Info'}</button>
+                                </div>
+                            </div>
+                    )
+                })}
+            </div>
+    )
+}
+
+export default Cards;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import '../styles/search.scss';
 import axios from 'axios';
-import { StarOutlined, StarFilled, Loading3QuartersOutlined } from '@ant-design/icons';
-import ReactHtmlParser from 'react-html-parser';
+
+import Cards from './Cards';
 
 function Search() {
     const [results, setResults] = useState([]);
@@ -54,19 +54,6 @@ function Search() {
         )
     }
 
-    // Ant Design Styles
-    const starIconStyle = {
-        fontSize: '2.5rem', 
-        height: '5vh',
-        cursor: 'pointer',
-        color: '#fcde7b',
-    }
-
-    const loadingIconStyle = {
-        fontSize: '3rem',
-        width: '100%'
-    }
-    
     return (
         <div className="search-content">
             <form onSubmit={submitSearch} className="search-form">
@@ -80,40 +67,7 @@ function Search() {
                 <button type="submit" className="search-button">Search</button>
             </form>
 
-            <div className="cards-wrapper">
-                { loading && <Loading3QuartersOutlined spin style={loadingIconStyle} />}
-                { results && results.map(recipe => {
-                    const { id, image, title, readyInMinutes, servings, spoonacularScore, vegan, vegetarian, pricePerServing } = recipe
-                    const description = recipe.summary
-                    return (
-                            <div className="card" key={id}>
-                                <img className="recipe-image" src={image} alt="recipe"/>
-                                <div className="card-content">
-                                    <h2 className="recipe-title">{title}</h2>
-                                    <div className="recipe-info">
-                                        <p>Prep-time < br />{readyInMinutes} Minutes</p>
-                                        <p>Servings < br />{servings}</p>
-                                        <p>Score < br />{spoonacularScore}</p>
-                                    </div>
-                                </div>
-
-                                { recipe.open && (
-                                    <div className='open'>
-                                        <div className='description'>
-                                            <h2>Description</h2>
-                                            <p>{ ReactHtmlParser(description) }</p>
-                                        </div>
-                                    </div>
-                                )}
-
-                                <div className="learn-more">
-                                    {recipe.liked ? (<StarFilled style={starIconStyle} onClick={() => likedRecipeHandler(id)} />) : (<StarOutlined style={starIconStyle} onClick={() => likedRecipeHandler(id)} /> ) }
-                                    <button onClick={() => moreInfoHandler(id)}>{ !recipe.open ? 'More Info' : 'Less Info'}</button>
-                                </div>
-                            </div>
-                    )
-                })}
-            </div>
+            <Cards recipes={results} loading={loading} likedRecipeHandler={likedRecipeHandler} moreInfoHandler={moreInfoHandler} />
         </div>
     )
 }
