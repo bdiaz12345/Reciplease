50fe874f2880690fcf6104832e2d2bb6df13f3fc
Austin Greer <agreer.ag12@gmail.com>
2022-01-08 20:57:23 -0500

    wip

--- client/src/components/AuthRoute.js
+++ client/src/components/AuthRoute.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 function AuthRoute({children})  {
     const auth = window.localStorage.getItem('token')
     
     return auth ? children : <Navigate to="/login" />
-    
 }
 
 export default AuthRoute
