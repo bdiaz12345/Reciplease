6547c1fe1a168bf3d434f89ac1450b01313690a8
Austin Greer <agreer.ag12@gmail.com>
2022-01-09 21:31:05 -0500

    form values are being tracked by state. Next step is to build the route in the api

--- client/src/components/ForgotPassword.js
+++ client/src/components/ForgotPassword.js
@@ -4,27 +4,37 @@ import { Link } from 'react-router-dom';
 import '../styles/forgot.scss'
 
 function ForgotPassword() {
-    const [disableBtn, setDisabledBtn] = useState(true)
+    const [inputValue, setInputValue] = useState("");
 
-    const enableBtn = () => {
-        setDisabledBtn(!disableBtn)
+    const inputChangeListener = (e) => {
+        setInputValue(e.target.value)
     }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log('handler initiated', inputValue)
+    }
+
     return (
         <div className="forgot-screen">
             <div className="forgot-wrapper">
                 <h1>Forgot Password</h1>
                 <p>Please provide your email address below so we can verify your account</p>
 
-                <form>
+                <form onSubmit={submitHandler}>
                     <input 
                         type='text'
                         name='email'
                         placeholder="Email"
                         className="forgot-input"
+                        value={inputValue}
+                        onChange={inputChangeListener}
                     />
                     <button type="submit" className="forgot-btn">Verify</button>
                 </form>
                 <Link to="/login" className='return-login'>Return to Login</Link>
+                <p>Don't have an account? <Link to='/signup' className='return-login'>Create one here</Link></p>
             </div>
         </div>
     )
